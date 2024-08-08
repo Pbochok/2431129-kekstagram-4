285dacf0f7d9a03001f50b720f0730a76daad10e
Бочок Павел <pbochok5@gmail.com>
2024-08-08 18:18:31 +0500

    module9-task2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { arrOfPhotoDescriptions } from './data.js';
 import { renderPictures } from './renderPicture.js';
 import './form.js';
+import './pictureFilters.js';
 
 renderPictures(arrOfPhotoDescriptions());
